d217d783d75d2dda80266a60865e20229b0c5704
Franklin Antony <36724845+franklin-antony@users.noreply.github.com>
2018-10-18 15:59:33 +0400

    Update simple-api/routes/routes.js
    Crashes the server when the "callWithDelayfunction" is called asynchronously. However the try/catch block is preventing the server from crashing.

--- simple-api/routes/routes.js
+++ simple-api/routes/routes.js
@@ -1,10 +1,27 @@
 var appRouter = function (app) {
   app.get("/WithError", function(req, res) {
 
-    throw new Error('An error occurred');
+    setTimeout(callWithDelayfunction, 5000);
+
     res.status(200).send("Successfull call to API");
   });
 
+
+  function callWithDelayfunction()
+   {
+    // stuff you want to happen
+
+    console.log('Logging to console on calling function with Delay');
+    try {
+      throw new Error('An error occurred');
+    } catch (e)
+    {
+      console.log(e);
+    } finally {
+
+    }
+
+  }
 
   app.get("/NoError", function(req, res) {
 
